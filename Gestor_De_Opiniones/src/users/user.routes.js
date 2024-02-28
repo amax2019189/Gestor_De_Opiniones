61d213253725cd1358ff209e58ca98a14a7c27ac
Alejandro <amax-2019189@kinal.edu.gt>
2024-02-28 00:24:03 -0600

    Se creo el usuario y ya se creo e post y put

--- Gestor_De_Opiniones/src/users/user.routes.js
+++ Gestor_De_Opiniones/src/users/user.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { check } from "express-validator";
 import {
-    usuariosGet,
+    usuariosPost,
     getUsuarioById,
+    usuariosPut,
 } from "./user.controller.js";
 import {
     existeEmail,
@@ -13,12 +14,33 @@ import { validarJWT } from "../middlewares/validar-jwt.js";
 
 const router = Router();
 
-router.get(
-    "/:id",
-    [
-      check("id", "It is not a valid ID").isMongoId(),
-      check("id").custom(existeUsuarioById),
-      validarCampos,
-    ],
-    getUsuarioById
-  );
+router.post(
+  "/",
+  [
+    check("nombre", "The name is required").not().isEmpty(),
+    check("password", "The password must be greater than 6 characters").isLength({
+      min: 6,
+    }),
+    check("correo", "This is not a valid email").isEmail(),
+    check("correo").custom(existeEmail),
+    validarCampos,
+  ],
+  usuariosPost
+);
+
+router.put(
+  "/:id",
+  [
+    check("id", "Not a valid ID").isMongoId(),
+    check("id").custom(existeUsuarioById),
+    check("correo", "Email is mandatory").isEmail(),
+    check("correo").custom(existeEmail),
+    check("password", "").isLength({
+      min: 6,
+    }),
+    validarCampos,
+  ],
+  usuariosPut
+);
+
+export default router;
